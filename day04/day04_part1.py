c26d2553747a2470d50606739c0ca254cf8e3736
Nick <nkemter@gmx.de>
2023-12-06 18:23:30 +0100

    cleaned up code

--- day04/day04_part1.py
+++ day04/day04_part1.py
@@ -1,5 +1,4 @@
 # Card 1: 41 48 83 86 17 | 83 86  6 31 17  -> 48 83 86 17 -> 8 punkte -> 1, 2, 4, 8
-# with open('test.txt', 'r') as file:
 with open('day04_input.txt', 'r') as file:
     total_points = 0
     for line in file:
@@ -9,11 +8,8 @@ with open('day04_input.txt', 'r') as file:
         array = [string.split(" ") for string in array]
         array[0] = list(filter(None, array[0])) #macht leere strings weg
         array[1] = list(filter(None, array[1]))
-        # print(array)
 
         for element in array[0]:
-            index = 0
-            mult = 0
             points = 0
             contained = [a in array[0] for a in array[1]]
             for bool in contained:
@@ -25,4 +21,3 @@ with open('day04_input.txt', 'r') as file:
                         points = points * 2
         total_points += points
     print(total_points)
-            
